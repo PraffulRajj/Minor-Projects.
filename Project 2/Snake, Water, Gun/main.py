c7e5f1a0924268d152cf07814e6d434698f76742
Prafful raj <praffulraj123@gmail.com>
2025-07-04 18:54:26 +0530

    Completed this game

--- Project 2/Snake, Water, Gun/main.py
+++ Project 2/Snake, Water, Gun/main.py
@@ -4,6 +4,35 @@
 0 for gun
 '''
 computer = -1
-you = input("Enter your choice: ")
+youstr = input("Enter your choice: ")
 youdict = {"s": 1, "w": -1, "g": 0}
-younum = youdict[you]
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+you = youdict[youstr]
+
+print(f"You choose {reverseDict[you]}\nComputer Choose {reverseDict[computer]}")
+
+if(computer == you):
+    print("Draw!")
+
+else:
+    if(computer == -1 and you == 1):
+        print("You Win!")
+
+    elif(computer == -1 and you ==0):
+        print("You Loose!")
+
+    elif(computer == 1 and you == -1):
+        print("You Loose!")
+
+
+    elif(computer == 1 and you == 0):
+        print("You Win!")
+
+    elif(computer ==0 and you ==-1):
+        print("You Win!")
+
+    elif(computer ==0 and you ==1):
+        print("You Loose!")
+
+    else:
+        print("Something went wrong!")    
